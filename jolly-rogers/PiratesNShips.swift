fcd1bd423f4d7bf42d3bfb06a178e827f5fe769b
Frazer MacRostie <frazermac101@outlook.com>
2020-01-10 14:14:34 +0000

    Adding in current Work

---
+++ jolly-rogers/PiratesNShips.swift
@@ -0,0 +1,44 @@
+//
+//  PiratesNShips.swift
+//  jolly-rogers
+//
+//  Created by Code Nation on 10/01/2020.
+//  Copyright © 2020 Code Nation. All rights reserved.
+//
+
+import Foundation
+
+class PirateCrew {
+    var CaptainsName: String?
+    var navSkill: Int!          //total of crew stats
+    var combatSkill: Int!       //total of crew stats
+    var morale: Int!            //total of crew stats
+    var loyalty: Int!           //total of crew stats
+    var experience: Int = 0
+    var ship: PirateShip?
+    var roster: Array<Any>!
+    
+//    init(roster: Array<Any>) {
+//        self.roster = roster
+//    }
+}
+
+class PirateShip {
+    var ShipsName: String?
+    var health: Int = 100       //percentage out of 100
+    var speed: Int = 60         // calculated between weight, ballast and condition
+    var ballast: Int = 0        //between -50 and 50
+    var condition: Int = 50     // #/100
+    
+}
+
+class Crewman {
+    var name: String!
+    var navSkill: Int!      // #/10
+    var combatSkill: Int!   // #/10
+    var morale: Int!        // #/10
+    var loyalty: Int!       // #/10
+}
+
+var currentCrewMembers =
+["Chance", "Bruno", "Toby", "Damien", "Rafael", "Domenic", "Weston", "Edison", "Chris", "Grant", "Cortez", "Darren", "Nicolas", "Everette", "Jason", "Vicente", "Noe", "Ricky", "Ivory", "Thomas", "Guillermo", "Isreal", "Ben", "Winfred", "Lincoln", "Kareem", "Antwan", "Greg", "Lawrence", "Corey", "Todd", "Jeffery", "Rayford", "Julio", "Manual", "Chi", "Bradford", "Devon", "Bryan", "Andreas", "Don", "Erik", "Bobbie", "Tyree", "Felipe", "Clifton", "Carrol", "Kasey", "Cliff", "Jack"]
--- jolly-rogersTests/jolly_rogersTests.swift
+++ jolly-rogersTests/jolly_rogersTests.swift
@@ -11,7 +11,44 @@ import XCTest
 
 class jolly_rogersTests: XCTestCase {
 
+    var crewNames =
+    ["Chance", "Bruno", "Toby", "Damien", "Rafael", "Domenic", "Weston", "Edison", "Chris", "Grant", "Cortez", "Darren", "Nicolas", "Everette", "Jason", "Vicente", "Noe", "Ricky", "Ivory", "Thomas", "Guillermo", "Isreal", "Ben", "Winfred", "Lincoln", "Kareem", "Antwan", "Greg", "Lawrence", "Corey", "Todd", "Jeffery", "Rayford", "Julio", "Manual", "Chi", "Bradford", "Devon", "Bryan", "Andreas", "Don", "Erik", "Bobbie", "Tyree", "Felipe", "Clifton", "Carrol", "Kasey", "Cliff", "Jack"]
+    
+    
+    var crewmenAll = [Crewman]?
+    
+    func generateCrew()
+    {
+        for _ in 1...25
+        {
+            let x = Int.random(in: 0..<50)
+            let name = crewNames[x]
+            var name:Crewman!
+            crewmenAll.append(name)
+            print(crewmenAll)
+        }
+    }
+    
+    
     override func setUp() {
+        
+        for _ in crewmenAll
+        {
+            let navInt = Int.random(int: 3...7)
+            let combatInt = Int.random(int: 3...7)
+            let moraleInt = Int.random(int: 3...7)
+            
+            name = Crewman(name: Name, navSkill: navInt, combatSkill: combatInt, morale: moraleInt, loyalty: 5)
+            
+            
+        }
+        
+//        var name: String!
+//        var navSkill: Int!      // #/10
+//        var combatSkill: Int!   // #/10
+//        var morale: Int!        // #/10
+//        var loyalty: Int!
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
